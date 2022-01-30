a149f58334ea6bf5988b39fc998727f90237d96d
serbolsd <32022302+serbolsd@users.noreply.github.com>
2022-01-30 04:34:58 -0600

    Cambio de dragon, ahora sigue a la y del jugador

--- Assets/Scripts/Enemy/Dragon.cs
+++ Assets/Scripts/Enemy/Dragon.cs
@@ -11,19 +11,22 @@ public class Dragon : MonoBehaviour
 
     private float offset;
 
+    public PlayerMovement m_player;
+
     void Start()
     {
         // Offset must be negative!
         offset = transform.position.x;
+        m_player = FindObjectOfType<PlayerMovement>();
     }
 
     void Update()
     {
         // Creep forward
         transform.position += new Vector3(speed * Time.deltaTime, transform.position.y);
-
-        // Snap to left camera edge
-        if (cam.position.x + offset > transform.position.x)
+    transform.position = new Vector3(transform.position.x, m_player.gameObject.transform.position.y, 0);
+    // Snap to left camera edge
+    if (cam.position.x + offset > transform.position.x)
         {
             transform.position = new Vector3(cam.transform.position.x + offset, transform.position.y);
         }
